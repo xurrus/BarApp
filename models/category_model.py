4f26907ec4d24c0c992278e8238ee2114f755d2b
xurrus <dgozalvezbarbera@gmail.com>
2023-01-10 03:38:27 -0500

    Category model improved

--- models/category_model.py
+++ models/category_model.py
@@ -8,9 +8,14 @@ class CategoryModel(models.Model):
                         'UNIQUE (name)',
                         'There cannot be two categories with the same name!!'),
                         ]
+    _rec_name = 'full_name'
+    _order = 'full_name'
 
     name = fields.Char(string="Name",help="Name of the category",requiered=True,index=True)
+    full_name = fields.Char(string="Full name",compute="_computeFullName")
     products = fields.One2many("bar_app.product_model","category",string="List of products")
+    photo = fields.Binary(string="Image")
+    parent_id = fields.Many2one("bar_app.category_model",string="Category parent",index=True)
     numProducts = fields.Integer(string="Number of products",help="Number of products in this category",compute="_totalProducts",store=True)
         
 
@@ -22,3 +27,11 @@ class CategoryModel(models.Model):
     @api.depends("products")
     def _totalProducts(self):
         self.numProducts = len(self.products)
+
+    @api.depends('name','parent_id.full_name')
+    def _computeFullName(self):
+        for category in self:
+            if category.parent_id:
+                category.full_name = " %s / %s" % (category.parent_id.full_name,category.name)
+            else:
+                category.full_name = category.name
